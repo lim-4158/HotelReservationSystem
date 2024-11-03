b8a421dc64099e4596a6aecd168e551431570863
jaiwee <weejiaen@gmail.com>
2024-11-03 20:32:40 +0800

    edited salesmanager session bean

--- HoRSLibrary/src/ejb/session/stateless/SalesManagerSessionBeanRemote.java
+++ HoRSLibrary/src/ejb/session/stateless/SalesManagerSessionBeanRemote.java
@@ -4,7 +4,12 @@
  */
 package ejb.session.stateless;
 
+import entity.RoomRate;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 import javax.ejb.Remote;
+import util.RoomRateTypeEnum;
 
 /**
  *
@@ -12,5 +17,25 @@ import javax.ejb.Remote;
  */
 @Remote
 public interface SalesManagerSessionBeanRemote {
+    
+    public Long createNewRoomRate(RoomRate roomRate);
+
+    public void updateRateName(String rateName, String roomType, String rateType, String newRateName);
+
+    public void updateRoomType(String rateName, String roomType, String rateType, String newRoomType);
+
+    public void updateRateType(String rateName, String roomType, String rateType, RoomRateTypeEnum rateTypeEnum);
+
+    public void updateRateAmount(String rateName, String roomType, String rateType, BigDecimal newAmount);
+
+    public void updateStartDate(String rateName, String roomType, String rateType, LocalDate newDate);
+
+    public void updateEndDate(String rateName, String roomType, String rateType, LocalDate newDate);
+
+    public void deleteRoomRate(String rateName);
+
+    public List<RoomRate> retrieveAllRoomRates();
+
+    public RoomRate retrieveRoomRateByName(String rateName);
     
 }
--- HoRSjpa-ejb/src/java/ejb/session/stateless/SalesManagerSessionBean.java
+++ HoRSjpa-ejb/src/java/ejb/session/stateless/SalesManagerSessionBean.java
@@ -4,7 +4,15 @@
  */
 package ejb.session.stateless;
 
+import entity.RoomRate;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import util.RoomRateTypeEnum;
 
 /**
  *
@@ -12,6 +20,75 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class SalesManagerSessionBean implements SalesManagerSessionBeanRemote, SalesManagerSessionBeanLocal {
+
+    @PersistenceContext(unitName = "HoRSjpa-ejbPU")
+    private EntityManager em;
+    
+    public Long createNewRoomRate(RoomRate roomRate) {
+        em.persist(roomRate);
+        em.flush();
+        
+        return roomRate.getRoomRateID();
+    }
+    
+    public void updateRateName(String rateName, String roomType, String rateType, String newRateName) {
+    
+    }
+    
+    public void updateRoomType(String rateName, String roomType, String rateType, String newRoomType) {
+    
+    }
+    
+    public void updateRateType(String rateName, String roomType, String rateType, RoomRateTypeEnum rateTypeEnum) {
+    
+    }
+    
+    public void updateRateAmount(String rateName, String roomType, String rateType, BigDecimal newAmount) {
+    
+    }
+    
+    public void updateStartDate(String rateName, String roomType, String rateType, LocalDate newDate) {
+    
+    }
+    
+    public void updateEndDate(String rateName, String roomType, String rateType, LocalDate newDate) {
+    
+    }
+    
+    
+    
+    
+    
+    public void deleteRoomRate(String rateName) {
+        RoomRate rate = retrieveRoomRateByName(rateName);
+                
+//        try {
+            
+            em.remove(rate);  // Delete the entity
+//        } catch (NoResultException e) {
+//            System.out.println("RoomRate not found for deletion.");
+//        }
+    }
+    
+    public List<RoomRate> retrieveAllRoomRates() {
+        return em.createQuery("SELECT r FROM RoomRate r", RoomRate.class).getResultList();
+    }
+    
+    
+    public RoomRate retrieveRoomRateByName(String rateName) {
+        Query q = em.createQuery("SELECT r FROM RoomRate r WHERE r.rateName = :rateName ");
+        q.setParameter("rateName", rateName);
+ 
+
+        return (RoomRate) q.getSingleResult();
+    }
+    
+    
+    
+    
+    
+    
+    
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
