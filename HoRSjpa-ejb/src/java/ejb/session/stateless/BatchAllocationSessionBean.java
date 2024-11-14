91ee2d1307337bbccf991630d1937b69d3845000
jaiwee <weejiaen@gmail.com>
2024-11-14 20:02:05 +0800

    pushing batch alloc

--- HoRSjpa-ejb/src/java/ejb/session/stateless/BatchAllocationSessionBean.java
+++ HoRSjpa-ejb/src/java/ejb/session/stateless/BatchAllocationSessionBean.java
@@ -29,53 +29,64 @@ public class BatchAllocationSessionBean implements BatchAllocationSessionBeanRem
     @PersistenceContext(unitName = "HoRSjpa-ejbPU")
     private EntityManager em;
     
+    
+    // Allocate Rooms method allocates the reservations for all reservations who check in on the date itself
     @Override
     public void allocateRooms(LocalDate date) {
         
+        // Allocate reservations with the highest priority room type first
         Query q = em.createQuery(
-            "SELECT r FROM Reservation r WHERE r.checkInDate = :checkInDate", Reservation.class).setParameter("checkInDate", date);
+        "SELECT r FROM Reservation r WHERE r.checkInDate = :checkInDate ORDER BY r.roomType.tierNumber DESC", Reservation.class);
+        q.setParameter("checkInDate", date);
         
         List<Reservation> reservations = q.getResultList();
         
         for (Reservation r : reservations) {
+            
+            // Get the desired Room Type
             RoomType rt = r.getRoomType();
             
+            // Get the desired number of Rooms 
+            int requiredRooms = r.getNumberOfRooms();
             
-            Map.Entry<Boolean, RoomReservation> allocated = allocate(rt, date, r);
+            // For each room, attempt to allocate
+            for (int i = 0; i < requiredRooms; i++) {
+                Map.Entry<Boolean, RoomReservation> allocated = allocate(rt, date, r);
             
-        
-            if (!allocated.getKey()) {
-                // TYPE 1: check for the other room types, allocate, and then generate exception report
-                boolean isType1 = false;
-                
-                
-                int tierNumber = rt.getTierNumber();
-                Query query = em.createQuery(
-                        "SELECT rt FROM RoomType rt WHERE rt.tierNumber > :currentTierNumber ORDER BY rt.tierNumber ASC", RoomType.class).setParameter("currentTierNumber", tierNumber);
+                if (!allocated.getKey()) {
+                    // TYPE 1: check for the other room types, allocate, and then generate exception report
+                    boolean isType1 = false;
 
-                List<RoomType> roomtypes = query.getResultList();
-                
-                for (RoomType rt1 : roomtypes) {
-                
-                    Map.Entry<Boolean, RoomReservation> nextAllocated = allocate(rt1, date, r);
-                    if (nextAllocated.getKey()) {
-                        isType1 = true;
-                        
-                        RoomReservation rv = nextAllocated.getValue();
-                        ExceptionReport er1 = new ExceptionReport(rv, ExceptionReportTypeEnum.TYPE1, LocalDate.now(), r.getReservationID());
-                        em.persist(er1);
-                        em.flush();
-                        break;
+
+                    int tierNumber = rt.getTierNumber();
+                    Query query = em.createQuery(
+                            "SELECT rt FROM RoomType rt WHERE rt.tierNumber > :currentTierNumber ORDER BY rt.tierNumber ASC", RoomType.class).setParameter("currentTierNumber", tierNumber);
+
+                    List<RoomType> roomtypes = query.getResultList();
+
+                    for (RoomType rt1 : roomtypes) {
+
+                        Map.Entry<Boolean, RoomReservation> nextAllocated = allocate(rt1, date, r);
+                        if (nextAllocated.getKey()) {
+                            isType1 = true;
+
+                            RoomReservation rv = nextAllocated.getValue();
+                            ExceptionReport er1 = new ExceptionReport(rv, ExceptionReportTypeEnum.TYPE1, LocalDate.now(), r.getReservationID());
+                            em.persist(er1);
+                            em.flush();
+                            break;
+                        }
+
                     }
-                
+
+                    // TYPE 2: check for the other room types, allocate, and then generate exception report
+                    if (!isType1) {
+                        ExceptionReport er2 = new ExceptionReport(ExceptionReportTypeEnum.TYPE2, LocalDate.now(), r.getReservationID());
+                        em.persist(er2);
+                        em.flush();
+                    }    
+
                 }
-                
-                // TYPE 2: check for the other room types, allocate, and then generate exception report
-                if (!isType1) {
-                    ExceptionReport er2 = new ExceptionReport(ExceptionReportTypeEnum.TYPE2, LocalDate.now(), r.getReservationID());
-                    em.persist(er2);
-                    em.flush();
-                }    
                 
             }
         
@@ -83,7 +94,7 @@ public class BatchAllocationSessionBean implements BatchAllocationSessionBeanRem
         
     }
     
-    public Map.Entry<Boolean, RoomReservation> allocate(RoomType rt, LocalDate date, Reservation r) {
+    private Map.Entry<Boolean, RoomReservation> allocate(RoomType rt, LocalDate date, Reservation r) {
         List<Room> rooms = rt.getRooms();
             
         boolean allocated = false;
@@ -93,9 +104,8 @@ public class BatchAllocationSessionBean implements BatchAllocationSessionBeanRem
             boolean exist = true;
 
             // get roomreservations
-            Query q1 = em.createQuery("SELECT rv FROM RoomReservation rv WHERE rv.room = :room AND rv.reservation = :reservation")
-                .setParameter("room", room)
-                .setParameter("reservation", r);
+            Query q1 = em.createQuery("SELECT rv FROM RoomReservation rv WHERE rv.room = :room")
+                .setParameter("room", room);
 
             List<RoomReservation> roomreservations = q1.getResultList();
 
--- HoRSjpa-ejb/src/java/ejb/session/stateless/BatchAllocationSessionBeanLocal.java
+++ HoRSjpa-ejb/src/java/ejb/session/stateless/BatchAllocationSessionBeanLocal.java
@@ -4,6 +4,7 @@
  */
 package ejb.session.stateless;
 
+import java.time.LocalDate;
 import javax.ejb.Local;
 
 /**
@@ -12,5 +13,6 @@ import javax.ejb.Local;
  */
 @Local
 public interface BatchAllocationSessionBeanLocal {
+    public void allocateRooms(LocalDate date);
     
 }
